520de9d3eb2fe0d1253718561e44fe7bcfcd9595
Jair Torezone <jairtorezone@gmail.com>
2024-04-03 06:57:23 -0300

    created TableHeader component

--- src/components/Attendee-list.tsx
+++ src/components/Attendee-list.tsx
@@ -8,6 +8,7 @@ import {
 } from "lucide-react";
 import { IconButton } from "./Icon-button";
 import { Table } from "./table/Table";
+import { TableHeader } from "./table/Table-header";
 
 export function AttendeeList() {
   return (
@@ -26,31 +27,17 @@ export function AttendeeList() {
       <Table>
         <thead>
           <tr className="border-b border-white/10">
-            <th
-              style={{ width: 48 }}
-              className="py-3 px-4 text-sm font-semibold text-left"
-            >
+            <TableHeader style={{ width: 48 }}>
               <input
                 type="checkbox"
                 className="size-4 bg-black/20 rounded border-white/10 "
               />
-            </th>
-            <th className="py-3 px-4 text-sm font-semibold text-left">
-              Código
-            </th>
-            <th className="py-3 px-4 text-sm font-semibold text-left">
-              Participante
-            </th>
-            <th className="py-3 px-4 text-sm font-semibold text-left">
-              Data de inscrição
-            </th>
-            <th className="py-3 px-4 text-sm font-semibold text-left">
-              Data do check-in
-            </th>
-            <th
-              style={{ width: 64 }}
-              className="py-3 px-4 text-sm font-semibold text-left"
-            ></th>
+            </TableHeader>
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participante</TableHeader>
+            <TableHeader>Data de inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader style={{ width: 64 }}></TableHeader>
           </tr>
         </thead>
 
